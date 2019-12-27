cdffeff88e9df2b2cb2adea8e9266a29d513f081
net-ka <a.k.fedosova@gmail.com>
2019-12-27 20:28:06 +0200

    Updates

---
+++ precache-manifest.2a6409e5fd31b439e5b1c7f3a5b9e4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4aa4c40d642954f970c5bdd003c43b6",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "55f6c8f4bc5583b8290a",
+    "url": "/game-in-dots/static/css/main.ad15fb93.chunk.css"
+  },
+  {
+    "revision": "9f2d4461defcba6309e0",
+    "url": "/game-in-dots/static/js/2.76ac3940.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-in-dots/static/js/2.76ac3940.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55f6c8f4bc5583b8290a",
+    "url": "/game-in-dots/static/js/main.17722ae9.chunk.js"
+  },
+  {
+    "revision": "a98c9ba37dca38e7ff90",
+    "url": "/game-in-dots/static/js/runtime-main.8b491ac7.js"
+  }
+]);
